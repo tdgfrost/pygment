6f24c3d5d0f0a35e7af65967db8874dff5abf268
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2022-11-24 18:41:38 +0000

    Now solved
    Only issue is that it solves with hard updates, but not sure whether it solves with soft updates (à la tau). For the target reward, set the max_episode_steps to 100-200 more than the target reward.

--- pygment/samples/CartPole DQN/CartPole DQN.py
+++ pygment/samples/CartPole DQN/CartPole DQN.py
@@ -2,11 +2,11 @@ import pygment as pm
 import gymnasium as gym
 from time import sleep
 
-env = gym.make('CartPole-v1')
+env = gym.make('CartPole-v1', max_episode_steps=700)
 agent = pm.create_agent('doubleDQN')
 agent.load_env(env)
 agent.add_network([64])
-agent.compile('adam', learning_rate=0.001)
-agent.train(500, tau=0.05)
+agent.compile('adam', learning_rate=0.01)
+agent.train(500, tau=200)
 
-pm.animate(agent, 'CartPole-v1')
+pm.animate(agent, 'CartPole-v1', max_episode_steps=700)
