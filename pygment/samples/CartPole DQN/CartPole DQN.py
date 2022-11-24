f72acf0344e69138e53010d6d176822ccb189efd
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2022-11-24 19:36:33 +0000

    .
    .

--- pygment/samples/CartPole DQN/CartPole DQN.py
+++ pygment/samples/CartPole DQN/CartPole DQN.py
@@ -5,8 +5,8 @@ from time import sleep
 env = gym.make('CartPole-v1', max_episode_steps=300)
 agent = pm.create_agent('doubleDQN')
 agent.load_env(env)
-agent.add_network([64])
+agent.add_network([32, 32])
 agent.compile('adam', learning_rate=0.001)
-agent.train(200, tau=0.001)
+agent.train(200, tau=0.001, gamma=0.999999)
 
 pm.animate(agent, 'CartPole-v1', max_episode_steps=300)
