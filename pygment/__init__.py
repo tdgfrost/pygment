89c3eb96552e47947a6786d60c68c66954053ff5
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-05-30 15:01:52 +0100

    Update __init__.py
    .

--- pygment/__init__.py
+++ pygment/__init__.py
@@ -1,21 +1,23 @@
-from .agent import DQNAgent, PolicyGradient, ActorCritic
+from .agent import DQNAgent, PolicyGradient, PPO, PPOContinuous
 import gymnasium as gym
 import os
+import torch
 
 
-def create_agent(agent_type='doubleDQN'):
-    agent_dict = {'doubleDQN': DQNAgent(),
-                  'actorcritic': ActorCritic(),
-                  'policy': PolicyGradient()}
+def create_agent(agent_type='doubledqn', device='mps'):
+    agent_dict = {'doubledqn': DQNAgent(device),
+                  'ppo': PPO(device),
+                  'policy': PolicyGradient(device),
+                  'ppocontinuous': PPOContinuous(device)}
 
-    if agent_type not in agent_dict.keys():
+    if agent_type.lower() not in agent_dict.keys():
         error = 'type must be one of: '
         for key in agent_dict.keys():
             error += key + ', '
         error = error[:-2]
         raise KeyError(error)
 
-    return agent_dict[agent_type]
+    return agent_dict[agent_type.lower()]
 
 
 def animate(agent, env_name, max_episode_steps=500, directory=None):
@@ -25,13 +27,22 @@ def animate(agent, env_name, max_episode_steps=500, directory=None):
     env = gym.make(env_name, render_mode='rgb_array', max_episode_steps=max_episode_steps)
     env = gym.wrappers.RecordVideo(env, directory)
     agent.load_env(env)
-    agent.net.to('mps')
+    #agent.net.to('mps')
+    if type(env.action_space) == gym.spaces.discrete.Discrete:
+        continuous = False
+    else:
+        continuous = True
 
     done = False
     state = agent.env.reset()[0]
     while not done:
-        action = agent.net(state)[0].item()
-        next_state, reward, done, _, _ = agent.env.step(action)
+        if continuous:
+          action_means, action_stds, _ = agent.net(state)
+          action, _, _ = agent.get_action_and_logprobs(action_means, action_stds)
+          action = torch.tanh(action).numpy()
+        else:
+          action = agent.net(state)[0].item()
+        next_state, _, _, _, _ = agent.env.step(action)
         state = next_state
 
     agent.env.close()
