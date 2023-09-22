Fixed IQL_train evaluate step; added input normalisation
1. Evaluate step in IQL_train was missing the correct environment
2. Added input normalisation

pygment/pygment/IQL_train.py
@@ -14,13 +14,13 @@ import wandb
 # Define config file - could change to FLAGS at some point
 config = {'seed': 123,
           'epochs': int(1e6),
-          'early_stopping': 200,
-          'value_batch_size': 1024,
-          'critic_batch_size': 1024,
-          'actor_batch_size': 1024 / (1-0.9),
+          'early_stopping': 1000,
+          'value_batch_size': 256,
+          'critic_batch_size': 256,
+          'actor_batch_size': int(256 / (1-0.9)),
           # Need to separate out batch sizes for value/critic networks (all data) and actor (only the subsampled data)
           'expectile': 0.9,
-          'gamma': 0.999,
+          'gamma': 0.99,
           'actor_lr': 0.001,
           'value_lr': 0.001,
           'critic_lr': 0.001,
@@ -60,9 +60,12 @@ if __name__ == "__main__":
 
     # Load static dataset
     print('Loading and processing dataset...')
-    data = load_data(path='./offline_datasets/LunarLander/dataset_4/dataset_combined.pkl',
+    data = load_data(path='./offline_datasets/LunarLander/dataset_2/dataset_reward_85.pkl',
                      scale='standardise',
                      gamma=config['gamma'])
+
+    # Standardise the state inputs
+    agent.standardise_inputs(data.states)
 
     # Make sure this matches with the desired dataset's extra_step metadata
     def extra_step_filter(x):
@@ -108,12 +111,14 @@ if __name__ == "__main__":
 
                 # Calculate next state values, discounted rewards (for critic update), and current advantages
                 next_state_values = agent.value(batch.next_states)[1] if is_net('critic') else None
-                rewards = calc_traj_discounted_rewards(batch.rewards, config['gamma'])
+                rewards = calc_traj_discounted_rewards(batch.rewards, config['gamma']) if is_net('critic') else None
 
                 advantages = (filter_to_action(jnp.minimum(*agent.critic(batch.states)[1]),
                                                batch.actions)
                               - agent.value(batch.states)[1]) if is_net('actor') else None
                 batch = alter_batch(batch, advantages=advantages)
+
+                # Standardise the advantages!
 
                 # Remove excess from the batch
                 excess_data = {}
@@ -173,7 +178,9 @@ if __name__ == "__main__":
 
         print('\n\n', '=' * 50, '\n', ' ' * 3, '\U0001F514' * 3, ' ' * 1, f'Evaluating network', ' ' * 2,
               '\U0001F514' * 3, '\n', '=' * 50)
-        results = evaluate_envs(agent, make_vec_env('LunarLander-v2', n_envs=envs_to_evaluate))
+        results = evaluate_envs(agent, make_vec_env(lambda: make_variable_env('LunarLander-v2',
+                                                                              fn=extra_step_filter),
+                                                    n_envs=envs_to_evaluate))
         print(f'\nMedian reward: {np.median(results)}')
 
         # Animate the agent's performance

pygment/pygment/core/common.py
@@ -19,7 +19,7 @@ Batch = namedtuple('Batch', fields, defaults=(None,) * len(fields))
 
 def load_data(path: str,
               scale: str,
-              gamma: float = 0.99) -> Dict[AnyStr, np.ndarray]:
+              gamma: float = 0.99) -> Batch:
     """
     Load data from path
     :param path: Provide a path to the folder containing the data

pygment/pygment/core/net.py
@@ -82,13 +82,16 @@ class ValueNet(nn.Module):
     activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
 
     @nn.compact
-    def __call__(self, observations: jnp.ndarray) -> tuple[dict[str, dict[int, Any]], Array]:
+    def __call__(self, observations: jnp.ndarray, input_mean=0, input_std=1) -> tuple[dict[str, dict[int, Any]], Array]:
         """
         Value network forward pass.
 
         :param observations: input data for the forward pass
         :return: output of the value network
         """
+
+        # Standardise the input data
+        observations = (observations - input_mean) / input_std
 
         # Do a forward pass with the MLP
         layer_outputs, value = MLP((*self.hidden_dims, 1),
@@ -113,7 +116,7 @@ class CriticNet(nn.Module):
     activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
 
     @nn.compact
-    def __call__(self, observations: jnp.ndarray) -> tuple[dict[str, dict[int, Any]], Any]:
+    def __call__(self, observations: jnp.ndarray, input_mean=0, input_std=1) -> tuple[dict[str, dict[int, Any]], Any]:
         """
         Critic network forward pass.
 
@@ -127,6 +130,10 @@ class CriticNet(nn.Module):
         
         Instead, we just treat the action as another input to the network.
         """
+        # Standardise the input data
+        observations = (observations - input_mean) / input_std
+
+        # Forward pass
         layer_outputs, critic = MLP((*self.hidden_dims, self.action_dims),
                                     activations=self.activations)(observations)
 
@@ -149,13 +156,15 @@ class DoubleCriticNet(nn.Module):
     activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
 
     @nn.compact
-    def __call__(self, observations: jnp.ndarray) -> tuple[dict[str, dict[int, Any]], tuple[Any, Any]]:
+    def __call__(self, observations: jnp.ndarray, input_mean=0, input_std=1) -> tuple[dict[str, dict[int, Any]], tuple[Any, Any]]:
         """
         Double critic network forward pass.
 
         :param observations: input data for each critic's forward pass
         :return: output of each critic network
         """
+        # Standardise the input data
+        observations = (observations - input_mean) / input_std
 
         # Forward pass for each critic network MLP
         layer_outputs_q1, critic1 = MLP((*self.hidden_dims, self.action_dims),
@@ -183,13 +192,15 @@ class ActorNet(nn.Module):
     activations: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
 
     @nn.compact
-    def __call__(self, observations: jnp.ndarray) -> tuple[dict[str, dict[int, Any]], Any]:
+    def __call__(self, observations: jnp.ndarray, input_mean=0, input_std=1) -> tuple[dict[str, dict[int, Any]], Any]:
         """
         Actor network forward pass.
 
         :param observations: input data for the forward pass
         :return: output of the actor network
         """
+        # Standardise the input data
+        observations = (observations - input_mean) / input_std
 
         # Forward pass with the MLP
         layer_outputs, logits = MLP((*self.hidden_dims, self.action_dims))(observations)
@@ -225,6 +236,8 @@ class Model:
     decay_rate: float = 0.99
     replacement_rate: float = 0.01
     maturity_threshold: int = 50
+    input_mean: float = 0
+    input_std: float = 1
 
     @classmethod
     def create(cls,
@@ -289,11 +302,16 @@ class Model:
         :return: output of the neural network
         """
 
-        return self.network.apply({'params': self.params}, *args)
+        return self.network.apply({'params': self.params}, *args,
+                                  input_mean=self.input_mean,
+                                  input_std=self.input_std)
 
     @jit
     def apply(self, *args, **kwargs):
-        return self.network.apply(*args, **kwargs)
+        return self.network.apply(*args,
+                                  input_mean=self.input_mean,
+                                  input_std=self.input_std,
+                                  **kwargs)
 
     def apply_gradient(self, loss_fn) -> Tuple['Model', Any]:
         """