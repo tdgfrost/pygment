752689088adbf5a8e8b919faf22c109d7f4ed74c
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-09-26 16:59:47 +0100

    Deactivated continual learning

--- pygment/core/net.py
+++ pygment/core/net.py
@@ -348,14 +348,14 @@ class Model:
                                                                        self.choose_features,
                                                                        ], info['layer_outputs'], new_params)
         """
-
+        """
         features_to_replace, num_features_to_replace = self.choose_features(outputs=info['layer_outputs'],
                                                                             new_params=new_params)
         
 
         # Update the new parameters with re-initialised low utility nodes, as required
         new_params = self.gen_new_features(features_to_replace, new_params)
-
+        """
         # Returns a COPY with the new parameters and optimiser state, as well as the metadata
         return self.replace(params=new_params,
                             opt_state=new_opt_state), info
