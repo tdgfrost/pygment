c97576a215c441906df20703e81a39c87f4b49bf
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-10-02 16:20:13 +0100

    Update quick_eval.py

--- pygment/quick_eval.py
+++ pygment/quick_eval.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     # Load static dataset
     print('Loading and processing dataset...')
-    baseline_reward = -2
-    data = load_data(path=f'./offline_datasets/LunarLander/dataset_1/dataset_reward_{baseline_reward}.pkl',
+    baseline_reward = 4
+    data = load_data(path=f'./offline_datasets/LunarLander/dataset_reward_{baseline_reward}.pkl',
                      scale='standardise',
                      gamma=config['gamma'])
 
