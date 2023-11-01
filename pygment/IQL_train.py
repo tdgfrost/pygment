510fa016acd96c1d7d06767b1edb52e44462b2e4
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-11-01 22:09:29 +0000

    .

--- pygment/IQL_train.py
+++ pygment/IQL_train.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     if logging_bool:
         wandb.init(
-            project="Cartpole-IQL",
+            project="CartPole-25pct-R-124",
             config=config,
         )
 
