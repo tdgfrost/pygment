e163b90ba86a37d6d4c1f096d8e14b3fcb2ec88e
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-11-20 10:28:46 +0100

    Fixed sh bug

--- pygment/generate_baseline.sh
+++ pygment/generate_baseline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define the values for soft_update and expectile
-step_delay=(0 1 2 3 4 5)
+step_delays=(0 1 2 3 4 5)
 
 # Loop over the soft_update values
-for soft_update in "${soft_updates[@]}"; do
+for step_delay in "${step_delays[@]}"; do
     python PPO_train.py --step-delay $step_delay
 done
