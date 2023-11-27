4d9788b5ccbbf893348205bdcdf6ff2f33cdab1f
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-11-27 11:07:57 +0000

    Update generate_dataset.py

--- pygment/generate_dataset.py
+++ pygment/generate_dataset.py
@@ -18,21 +18,21 @@ if __name__ == "__main__":
     # ============================================================== #
 
     # Load previous checkpoints
-    reward = 327
+    reward = 0
     probability = 0.25
-    steps = 2
-    dirname = f'./experiments/CartPole/{probability}_probability/PPO/{steps}-steps'
+    steps = 11
+    dirname = f'./experiments/{probability}_probability/PPO/insert_model_dir_here'
     model_checkpoints = os.path.join(dirname, 'model_checkpoints')
-    target_directory = f'./offline_datasets/CartPole/{probability}_probability'
+    target_directory = f'./offline_datasets/LunarLander/{probability}_probability'
 
     with open(os.path.join(dirname, 'config.txt'), 'r') as f:
         config = eval(f.read())
 
-    config['max_episode_steps'] = 500
+    config['max_episode_steps'] = 1000
     config['n_episodes'] = 10000
 
     # Create agent
-    env = make_env('CartPole-v1', max_episode_steps=config['max_episode_steps'])
+    env = make_env('LunarLander-v2', max_episode_steps=config['max_episode_steps'])
     agent = PPOAgent(observations=env.observation_space.sample(),
                      action_dim=env.action_space.n,
                      opt_decay_schedule="cosine",
@@ -43,15 +43,15 @@ if __name__ == "__main__":
 
     # Create variable environment template (optional)
     def extra_step_filter(x):
-        # If tilted to the left
-        if x[2] < 0:
-            # with p == 0.25, delay by a further n steps
-            if np.random.uniform() < 0.25:
+        # If in rectangle
+        if config['bottom_bar_coord'] < x[1] < config['top_bar_coord']:
+            # with p == 0.05, delay by 20 steps
+            if np.random.uniform() < probability:
                 return steps
         # Otherwise, normal time steps (no delay)
         return 0
 
-    envs = make_vec_env(lambda: make_variable_env('CartPole-v1', fn=extra_step_filter,
+    envs = make_vec_env(lambda: make_variable_env('LunarLander-v2', fn=extra_step_filter,
                                                   max_episode_steps=config['max_episode_steps']),
                         n_envs=config['n_episodes'])
 
